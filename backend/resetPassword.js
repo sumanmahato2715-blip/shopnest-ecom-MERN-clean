a894019328fa7888a020c4a685b4ef56a6771d0b
Suman Kumari <sumanmahato2715@gmail.com>
2026-07-25 12:10:23 +0530

    Add Netlify redirects

---
+++ backend/resetPassword.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
+const User = require("./models/User");
+
+async function resetPassword() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    const hashedPassword = await bcrypt.hash("123456", 10);
+
+    const result = await User.updateOne(
+      { email: "sumanmahato2715" },
+      { $set: { password: hashedPassword } }
+    );
+
+    console.log(result);
+    console.log("Password changed to: 123456");
+
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+resetPassword();
